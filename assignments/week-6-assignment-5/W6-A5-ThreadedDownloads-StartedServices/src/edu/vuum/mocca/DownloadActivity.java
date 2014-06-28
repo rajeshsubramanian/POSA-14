09eb2035c494fd2791bf7c814bb69d93c34866b8
Rajesh Subramanian <rsubramanian@resource.com>
2014-06-28 17:51:49 -0400

    Week 6 assignment 5 solution

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,8 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -78,6 +80,9 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+				Bundle data = msg.getData();
+				String pathname = data.getString(DownloadUtils.PATHNAME_KEY);
+				activity.displayBitmap(pathname);
             }
     	}
     }
@@ -107,6 +112,9 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
+			Intent downloadIntent = DownloadIntentService.makeIntent(this,
+					handler, getUrlString());
+			startService(downloadIntent);
 
             which = "Starting DownloadIntentService";
             break;
@@ -115,6 +123,10 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
+			Intent threadPoolIntent = ThreadPoolDownloadService.makeIntent(
+					this, handler,
+					getUrlString());
+			startService(threadPoolIntent);
 
             which = "Starting ThreadPoolDownloadService";
             break;
--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadIntentService.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadIntentService.java
@@ -63,7 +63,8 @@ public class DownloadIntentService extends IntentService {
     	// factory method in DownloadUtils that makes a Messenger
     	// Intent with the appropriate parameters.
 
-        return null;
+		return DownloadUtils.makeMessengerIntent(context,
+				DownloadIntentService.class, handler, uri);
     }
 
     /**
@@ -86,5 +87,8 @@ public class DownloadIntentService extends IntentService {
         // method from the DownloadUtils class that downloads the uri
         // in the intent and returns the file's pathname using a
         // Messenger who's Bundle key is defined by DownloadUtils.MESSENGER_KEY
+		Messenger messenger = (Messenger) intent.getExtras().get("MESSENGER");
+		DownloadUtils.downloadAndRespond(getApplicationContext(),
+				intent.getData(), messenger);
     }
 }
